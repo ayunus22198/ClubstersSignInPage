91e9eec7a9b4bb0ce7d7490e3a552f8d849659d4
Adnan <ayunus@ucsc.edu>
2019-05-09 01:37:13 -0700

    Updates

---
+++ precache-manifest.ead4343ceea83b81c14e191178a044f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfed0d17545cba669e1d9b2eb8c032d9",
+    "url": "/ClubstersSignInPage/index.html"
+  },
+  {
+    "revision": "0ab5ce90e55d41dbbec5",
+    "url": "/ClubstersSignInPage/static/css/2.7933c206.chunk.css"
+  },
+  {
+    "revision": "e64df20cbc8636bef892",
+    "url": "/ClubstersSignInPage/static/css/main.6458a6c8.chunk.css"
+  },
+  {
+    "revision": "0ab5ce90e55d41dbbec5",
+    "url": "/ClubstersSignInPage/static/js/2.e64f1364.chunk.js"
+  },
+  {
+    "revision": "e64df20cbc8636bef892",
+    "url": "/ClubstersSignInPage/static/js/main.8a03fae6.chunk.js"
+  },
+  {
+    "revision": "10dec13c6fad4ca5e257",
+    "url": "/ClubstersSignInPage/static/js/runtime~main.01d9b5c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ClubstersSignInPage/static/media/logo.5d5d9eef.svg"
+  }
+]);
